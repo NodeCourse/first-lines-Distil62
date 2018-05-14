5a7889e574eb13095d8fa4c42352ae18f0dd2d30
Distil62 <Distil62@github.com>
2018-05-14 09:51:20 +0200

    addAppend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
-const file = fs.createWriteStream('./file.txt');
+const file = fs.createWriteStream('./file.txt', {'flags' : 'a'});
 
 setInterval(()=>{
-    file.write(new Date().getTime() + "\n");
+    file.write(new Date().getTime() + "\n", );
 }, 1000);
